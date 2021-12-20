32806514023884a8596948373c3cd9b1a3ee7d06
Miikhaaeel <fadowomichael1@gmail.com>
2021-12-20 18:40:15 +0100

    HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
+import 'package:budget_app_ui/screens/homescreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Flutter Budget UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.green,
+      ),
+      home: HomeScreen(),
+    );
   }
 }
