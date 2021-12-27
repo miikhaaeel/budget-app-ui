f36a07c092d708ea5dc31f56d9c0744821605333
Miikhaaeel <fadowomichael1@gmail.com>
2021-12-27 23:55:23 +0100

    category list cards

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,9 +1,51 @@
 import 'package:budget_app_ui/data/data.dart';
+import 'package:budget_app_ui/models/category_model.dart';
 import 'package:budget_app_ui/widgets/bar_chart.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  _buildCategory(Category category, double totalAmountSpent) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      padding: const EdgeInsets.all(20.0),
+      height: 100,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, 2),
+            blurRadius: 6.0,
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                category.name!,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+              Text(
+                '\$${(category.maxAmount! - totalAmountSpent).toStringAsFixed(2)}/\$${category.maxAmount}',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,21 +85,30 @@ class HomeScreen extends StatelessWidget {
           SliverList(
             delegate: SliverChildBuilderDelegate(
               (context, index) {
-                return Container(
-                  margin: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black26,
-                        offset: Offset(0, 2),
-                        blurRadius: 6.0,
-                      ),
-                    ],
-                  ),
-                  child: BarChart(expenses: weeklySpending),
-                );
+                if (index == 0) {
+                  return Container(
+                    margin: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black26,
+                          offset: Offset(0, 2),
+                          blurRadius: 6.0,
+                        ),
+                      ],
+                    ),
+                    child: BarChart(expenses: weeklySpending),
+                  );
+                } else {
+                  final Category category = categories[index - 1];
+                  double totalAmountSpent = 0.0;
+                  for (var expense in category.expenses!) {
+                    totalAmountSpent += expense.cost!;
+                  }
+                  return _buildCategory(category, totalAmountSpent);
+                }
               },
               childCount: 1 + categories.length,
             ),
